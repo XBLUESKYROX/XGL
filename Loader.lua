a49a84e3a8d81028dc19fcac59944f6cdc824424
Zypher <96961791+XBLUESKYROX@users.noreply.github.com>
2022-03-25 15:16:15 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,23 @@
+--[[
+
+███████╗██╗░░░██╗██████╗░██╗░░██╗███████╗██████╗░██╗░░██╗
+╚════██║╚██╗░██╔╝██╔══██╗██║░░██║██╔════╝██╔══██╗╚██╗██╔╝
+░░███╔═╝░╚████╔╝░██████╔╝███████║█████╗░░██████╔╝░╚███╔╝░
+██╔══╝░░░░╚██╔╝░░██╔═══╝░██╔══██║██╔══╝░░██╔══██╗░██╔██╗░
+███████╗░░░██║░░░██║░░░░░██║░░██║███████╗██║░░██║██╔╝╚██╗
+╚══════╝░░░╚═╝░░░╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝
+
+]]
+
+
+if game.PlaceId == 4520749081 then -- King Legacy World 1
+    loadstring(game:HttpGet("https://zypherx.dekdee.repl.co/Script/Script/Script/4520749081.lua", true))()
+end
+
+elseif game.PlaceId == 4520749081 then -- King Legacy World 2
+    loadstring(game:HttpGet("https://zypherx.dekdee.repl.co/Script/Script/Script/4520749081.lua", true))()
+end
+
+else
+    loadstring(game:HttpGet("https://zypherx.dekdee.repl.co/Script/Script/Script/WrongGame", true))()
+end
